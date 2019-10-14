ecc2258ed697fa34a65acaf616dae63d72b96343
Thomas Bouder <tbouder@protonmail.com>
2019-10-14 18:37:54 +0200

    Creation of Mut

---
+++ Mut.go
@@ -0,0 +1,53 @@
+/*******************************************************************************
+** @Author:					Thomas Bouder <Tbouder>
+** @Email:					Tbouder@protonmail.com
+** @Date:					Monday 14 October 2019 - 17:54:41
+** @Filename:				Mutex.go
+**
+** @Last modified by:		Tbouder
+** @Last modified time:		Monday 14 October 2019 - 18:37:30
+*******************************************************************************/
+
+package		mut
+
+import		"sync"
+import		"github.com/microgolang/logs"
+
+var		MUTEXES = map[string]MutArr{}
+var		LOGS = false
+
+type	MutArr struct {
+	Mut		*sync.Mutex
+	uid		string
+}
+
+func	SetLogs(shouldSet bool) {
+	LOGS = shouldSet
+}
+
+func	get(uid string) MutArr {
+	_, ok := MUTEXES[uid]
+	if (!ok) {
+		MUTEXES[uid] = MutArr{
+			Mut: &sync.Mutex{},
+			uid: uid,
+		}
+	}
+	return (MUTEXES[uid])
+}
+
+func	Lock(uid string) {
+	Element := get(uid)
+	Element.Mut.Lock()
+	if (LOGS) {
+		logs.Info(`Lock ` + uid)
+	}
+}
+
+func	Unlock(uid string) {
+	Element := get(uid)
+	Element.Mut.Unlock()
+	if (LOGS) {
+		logs.Info(`Unlock ` + uid)
+	}
+}
